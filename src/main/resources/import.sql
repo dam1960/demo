fd0649725eeb9042f198b4e764e79247118bdd4b
David A. Miranda <dmiranda@midtowncg.com>
2020-07-01 10:57:02 -0600

    more changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,4 @@
-insert into customer (id, first_name, last_name, email) values(1, 'David', 'Miranda', 'test@gmail.com'), (2, 'Biff', 'Barfnagel', 'bbarfnagel@gmail.com'), (3, 'Homer', 'Simpson', 'homer.simpson@gmail.com'), (4, 'Marge', 'Simpson', 'msimpson@gmail.com');
+insert into customer (id, first_name, last_name, email) values(1, 'David', 'Miranda', 'test@gmail.com');
+insert into customer (id, first_name, last_name, email) values(2, 'Biff', 'Barfnagel', 'bbarfnagel@gmail.com');
+insert into customer (id, first_name, last_name, email) values(3, 'Homer', 'Simpson', 'homer.simpson@gmail.com');
+insert into customer (id, first_name, last_name, email) values(4, 'Marge', 'Simpson', 'msimpson@gmail.com');
